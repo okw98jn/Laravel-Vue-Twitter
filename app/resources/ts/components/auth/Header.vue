7106bcf308cbfa5c39fb7134ec4dd9827038b82f
okawa <0cu2045d@gmail.com>
2024-01-17 00:38:23 +0900

    フォームを中央に変更

--- app/resources/ts/components/auth/Header.vue
+++ app/resources/ts/components/auth/Header.vue
@@ -8,7 +8,7 @@ const { title, subTitle, routeName } = defineProps<Props>()
 </script>
 
 <template>
-    <div>
+    <div class="mt-12">
         <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
             alt="Workflow" />
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
