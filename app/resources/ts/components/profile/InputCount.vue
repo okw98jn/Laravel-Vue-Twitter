ff41202e985c6bd2e56cf67bd04e3308e5e9fafa
okawa <0cu2045d@gmail.com>
2024-01-29 00:06:30 +0900

    編集画面作成

---
+++ app/resources/ts/components/profile/InputCount.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+type Props = {
+    name: string;
+    label: string;
+    placeholder: string;
+    maxCount: string;
+}
+const { name, label, placeholder, maxCount } = defineProps<Props>();
+const model = defineModel<string>();
+const isFocused = ref(false);
+</script>
+
+<template>
+    <div class="w-full mx-auto py-2">
+        <div class="flex flex-col space-y-1">
+            <label :for="name" class="text-sm font-medium leading-none flex justify-between">
+                {{ label }}
+                <span v-if="isFocused" class="text-sm font-normal">{{ model?.length }} / {{ maxCount }}</span>
+                <span v-else class="text-sm font-normal text-white">none</span>
+            </label>
+            <input :id="name" :name="name" type="text" :maxlength="maxCount" :placeholder="placeholder" v-model="model"
+                @focus="isFocused = true" @blur="isFocused = false"
+                class="border p-2 rounded-md focus:outline-none focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1" />
+        </div>
+    </div>
+</template>
--- app/resources/ts/components/profile/UserEdit.vue
+++ app/resources/ts/components/profile/UserEdit.vue
@@ -3,6 +3,8 @@ import image from '@/assets/test.png';
 import Button from '@/components/ui/Button.vue';
 import { ref } from 'vue';
 import Modal from '@/components/ui/Modal.vue';
+import InputCount from './InputCount.vue';
+import TextAreaCount from './TextAreaCount.vue';
 
 const isOpen = ref(false);
 
@@ -14,19 +16,31 @@ const handleModalClose = (): void => {
     isOpen.value = false;
 }
 
+const user = ref({
+    name: '',
+    introduction: '',
+    location: '',
+});
 
 </script>
 
 <template>
     <div class="flex items-center justify-between py-4 -mt-20 px-4">
-        <img :src="image" :alt="image" class="w-32 h-32 object-cover rounded-full border-4 border-white">
+        <img :src="image" alt="image" class="w-32 h-32 object-cover rounded-full border-4 border-white">
         <div class="mt-12">
-            <Button text="Edit profile" @click="handleClick"
-                className="inline-flex rounded-full items-center justify-center text-sm font-medium border bg-white h-10 px-4 py-2 mt-6 hover:bg-gray-200 " />
+            <Button text="編集" @click="handleClick"
+                class-name="inline-flex items-center justify-center text-sm font-medium border bg-white h-10 px-4 py-2 mt-6 hover:bg-gray-200 " />
         </div>
         <Modal :isOpen="isOpen" title="プロフィール編集" @click="handleModalClose">
-            <div class="bg-gray-300">
-                <p>テスト</p>
+            <div class="w-full mx-auto py-2 px-4">
+                <InputCount name="name" label="名前" placeholder="名前" maxCount="50" v-model="user.name" />
+                <TextAreaCount name="introduction" label="自己紹介" placeholder="自己紹介" maxCount="160"
+                    v-model="user.introduction" />
+                <InputCount name="location" label="場所" placeholder="場所" maxCount="30" v-model="user.location" />
+                <div class="flex justify-end">
+                    <Button text="保存"
+                        class-name="w-20 mt-4 inline-flex items-center justify-center h-10 px-4 py-2 text-white bg-indigo-500 hover:bg-indigo-600" />
+                </div>
             </div>
         </Modal>
     </div>
