ae67100556ce1ba6885b678c8d0f667e0e7221f3
okawa <0cu2045d@gmail.com>
2024-01-30 02:09:02 +0900

    生年月日をアクセサでフォーマット

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'birthday',
     ];
 
     /**
@@ -40,4 +42,11 @@ class User extends Authenticatable
     protected $casts = [
         'password' => 'hashed',
     ];
+
+    protected function birthDay(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->format('Y年m月d日'),
+        );
+    }
 }
