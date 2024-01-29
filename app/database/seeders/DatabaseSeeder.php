8ae12fc135aa212cfc97fb9010130d02dd3febf2
okawa <0cu2045d@gmail.com>
2024-01-30 01:00:19 +0900

    テストデータ修正

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,6 +22,7 @@ class DatabaseSeeder extends Seeder
             'location'     => '日本/大阪',
             'birthday'     => '1997-09-08',
             'email'        => 'test@example.com',
+            'password'     => Hash::make('1234'),
         ]);
     }
 }
