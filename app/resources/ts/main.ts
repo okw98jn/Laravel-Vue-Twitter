aa7f0f0ea84c9d27ac72edb7baa98c512194c1b9
okawa <0cu2045d@gmail.com>
2024-01-31 01:29:18 +0900

    vue3-toastifyインストール

--- app/resources/ts/main.ts
+++ app/resources/ts/main.ts
@@ -4,10 +4,13 @@ import { createPinia } from 'pinia'
 import './index.css'
 import router from "./router";
 import VueElementLoading from "vue-element-loading";
+import Vue3Toastify from "vue3-toastify";
+import 'vue3-toastify/dist/index.css';
 
 const app = createApp(App);
 
 app.component("VueElementLoading", VueElementLoading);
-app.use(createPinia())
+app.use(Vue3Toastify);
+app.use(createPinia());
 app.use(router)
 app.mount("#app");
