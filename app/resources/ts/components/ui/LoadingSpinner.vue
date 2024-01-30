33f9ce35928d3e0f85ab4e6ac8a40ff10556a1e4
okawa <0cu2045d@gmail.com>
2024-01-31 00:52:09 +0900

    vue-element-loadingインストール

--- app/resources/ts/components/ui/LoadingSpinner.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-    <div class="animate-spin w-6 h-6 border-[3px] border-current border-t-transparent text-indigo-600 rounded-full">
-        <span class="sr-only">Loading...</span>
-    </div>
-</template>
--- app/resources/ts/main.ts
+++ app/resources/ts/main.ts
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import { createPinia } from 'pinia'
 import './index.css'
 import router from "./router";
+import VueElementLoading from "vue-element-loading";
 
 const app = createApp(App);
 
+app.component("VueElementLoading", VueElementLoading);
 app.use(createPinia())
 app.use(router)
 app.mount("#app");
