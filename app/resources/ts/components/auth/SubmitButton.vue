7ed0e44b422d74aee73a9844b43d10e3dba07073
okawa <0cu2045d@gmail.com>
2024-02-02 00:10:37 +0900

    ローディング時のエラー解消

--- app/resources/ts/components/auth/SubmitButton.vue
+++ app/resources/ts/components/auth/SubmitButton.vue
@@ -13,11 +13,10 @@ const { text, isLoading } = defineProps<Props>();
         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white transition duration-300 ease-in-out focus:outline-none"
         :class="isLoading ? 'bg-gray-300' : 'bg-indigo-600 hover:bg-indigo-700'">
         <template v-if="isLoading">
-            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 24 24">
+            <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042.
-                    1.135 5.824 3 7.938l3-2.647z">
+                <path class="opacity-75" fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
                 </path>
             </svg>
         </template>
