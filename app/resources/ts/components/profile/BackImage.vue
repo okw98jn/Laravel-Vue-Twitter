449ebd112e6698e373874e04eddfdfca0c12e980
okawa <0cu2045d@gmail.com>
2024-02-02 01:20:40 +0900

    画像ローディング時修正

--- app/resources/ts/components/profile/BackImage.vue
+++ app/resources/ts/components/profile/BackImage.vue
@@ -5,10 +5,12 @@ import { ComputedRef, computed } from 'vue';
 
 const store = useUserStore();
 const user: ComputedRef<UserStore> = computed(() => store.user);
+const isLoading: ComputedRef<boolean> = computed(() => store.isLoading);
 </script>
 
 <template>
-    <div>
+    <div v-if="isLoading" class="h-60"></div>
+    <div v-else>
         <img v-if="user.data.header_image" :src="user.data.header_image" alt="image" class="w-full object-cover h-60">
         <h2 v-else class="w-full bg-slate-200 h-60 rounded-sm"></h2>
     </div>
