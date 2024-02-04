6c17511b4fa094e9b37fdd0b4a55f778af821089
okawa <0cu2045d@gmail.com>
2024-02-04 15:55:21 +0900

    vue-datepickerインストール

--- app/resources/ts/main.ts
+++ app/resources/ts/main.ts
@@ -6,10 +6,13 @@ import router from "./router";
 import VueElementLoading from "vue-element-loading";
 import Vue3Toastify from "vue3-toastify";
 import 'vue3-toastify/dist/index.css';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App);
 
 app.component("VueElementLoading", VueElementLoading);
+app.component('VueDatePicker', VueDatePicker);
 app.use(Vue3Toastify);
 app.use(createPinia());
 app.use(router)
