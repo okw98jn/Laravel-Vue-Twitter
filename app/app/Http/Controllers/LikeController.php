6bffc325ca8226bf3a5f92d20a68db83225c203c
okawa <0cu2045d@gmail.com>
2024-02-09 01:30:28 +0900

    いいねのレスポンスコードを定数化

--- app/app/Http/Controllers/LikeController.php
+++ app/app/Http/Controllers/LikeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Tweet;
 use App\Services\Like\LikeService;
 use App\Services\Like\UnLikeService;
+use Illuminate\Http\Response;
 
 class LikeController extends Controller
 {
@@ -12,13 +13,13 @@ class LikeController extends Controller
     {
         $likeService->like($tweet);
 
-        return response()->json([], 204);
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 
     public function unlike(Tweet $tweet, UnLikeService $UnlikeService)
     {
         $UnlikeService->unLike($tweet);
 
-        return response()->json([], 204);
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
