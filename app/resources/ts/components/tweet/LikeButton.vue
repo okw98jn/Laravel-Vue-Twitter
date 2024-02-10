3260ea1502274af972a97c38710176a7b791dbce
okawa <0cu2045d@gmail.com>
2024-02-10 14:23:50 +0900

    いいねボタンの挙動修正

--- app/resources/ts/components/tweet/LikeButton.vue
+++ app/resources/ts/components/tweet/LikeButton.vue
@@ -16,32 +16,9 @@ const emit = defineEmits<{
 <template>
     <div :class="`flex items-center hover:text-red-500`" @click="emit('click')">
         <div class="p-1.5 rounded-full cursor-pointer transition duration-300 ease-in-out" :class="`hover:bg-red-100`">
-            <transition name="bounce">
-                <HeartIconSolid v-if="isLikedUser" class="h-5 w-5 text-rose-500" />
-            </transition>
-            <HeartIconOutline v-if="!isLikedUser" class="h-5 w-5" />
+            <HeartIconSolid v-if="isLikedUser" class="h-5 w-5 text-rose-500" />
+            <HeartIconOutline v-else class="h-5 w-5" />
         </div>
         <span class="text-xs -ml-1">{{ count }}</span>
     </div>
 </template>
-
-
-<style>
-.bounce-enter-active {
-    animation: bounce-in 0.5s;
-}
-
-@keyframes bounce-in {
-    0% {
-        transform: scale(0);
-    }
-
-    50% {
-        transform: scale(1.2);
-    }
-
-    100% {
-        transform: scale(1);
-    }
-}
-</style>
