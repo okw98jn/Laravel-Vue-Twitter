7a5beb7c7775aec2c38f90843ad2ea90f3c5ee46
okawa <0cu2045d@gmail.com>
2024-02-26 23:12:39 +0900

    プロフィールページへのリンク修正

--- app/resources/ts/components/tweet/TweetUser.vue
+++ app/resources/ts/components/tweet/TweetUser.vue
@@ -20,9 +20,11 @@ const route = useRoute()
         <div class="mb-1">
             <UserDetail :name="user.name" :user-id="user.user_id" :created="tweet.created" :id="tweet.user_id" />
             <p v-if="tweet.reply_user && route.name !== 'TweetDetail'" class="cursor-default">
-                <span class="text-indigo-500">
-                    {{ tweet.reply_user.user_id }}
-                </span>
+                <RouterLink :to="{ name: 'UserTweetList', params: { userId: tweet.reply_user.id } }">
+                    <span class="text-indigo-500 hover:underline">
+                        {{ tweet.reply_user.user_id }}
+                    </span>
+                </RouterLink>
                 <span class="text-gray-500">
                     に返信
                 </span>
--- app/resources/ts/views/Profile.vue
+++ app/resources/ts/views/Profile.vue
@@ -4,17 +4,25 @@ import UserDetail from '@/components/profile/UserDetail.vue';
 import UserEdit from '@/components/profile/UserEdit.vue';
 import BackImage from '@/components/profile/BackImage.vue';
 import { useProfileStore } from '@/stores/profile';
-import { onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import TweetTab from '@/components/profile/TweetTab.vue';
 
+const route = useRoute();
+let userId = route.params.userId as string;
+
+const profileStore = useProfileStore();
+
 onMounted(async () => {
-    const route = useRoute();
-    const userId = route.params.userId as string;
-    const profileStore = useProfileStore();
     await profileStore.fetchProfile(userId);
 });
 
+watch(() => route.params.userId, async (newUserId) => {
+    if (newUserId !== userId) {
+        userId = newUserId as string;
+        await profileStore.fetchProfile(userId);
+    }
+});
 </script>
 
 <template>
