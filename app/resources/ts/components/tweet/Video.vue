4de73351f1a7e6940b624c0fc2d9e12a3c28eb2a
okawa <0cu2045d@gmail.com>
2024-02-28 16:29:19 +0900

    不要コード削除

--- app/resources/ts/components/tweet/Video.vue
+++ app/resources/ts/components/tweet/Video.vue
@@ -12,22 +12,9 @@ const { id, videoPath } = defineProps<Props>();
 const tweetModalStore = useTweetModalStore();
 const isTweetModalOpen = computed(() => tweetModalStore.isTweetModalOpen);
 
-const videoRef = ref<HTMLVideoElement | null>(null);
-
-onMounted(() => {
-    videoRef.value = document.getElementById('videoElement_' + id) as HTMLVideoElement;
-});
-
-const toggleVideoPlayback = () => {
-    if (videoRef.value?.paused) {
-        videoRef.value.play();
-    } else {
-        videoRef.value?.pause();
-    }
-};
 </script>
 
 <template>
-    <video :id="`videoElement_${id}`" class="w-full h-96 object-cover rounded-lg" :class="{ '-z-10': isTweetModalOpen }"
-        controls :src="videoPath" @click="toggleVideoPlayback"></video>
+    <video class="w-full h-96 object-cover rounded-lg" :class="{ '-z-10': isTweetModalOpen }" controls
+        :src="videoPath"></video>
 </template>
